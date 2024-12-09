42b3cb4324d6891c4998a3279c0865022a879dc6
bowerbirdblue <levistvalentine@gmail.com>
2024-12-09 13:35:26 -0500

    fixes text issues

--- SAISB.py
+++ SAISB.py
@@ -23,11 +23,12 @@ font =("Autour One")
 leftFrame = tk.Frame(root, width=400, height=300, relief="raised", borderwidth=5) # creates text entry frame
 leftFrame.place(anchor="nw", x=0, y=0, width=400, height=300) # places text entry frame (top left)
 
-subFrame = tk.Frame(root, width=200, height=50, relief="sunken", borderwidth=5) # creates submit button frame
-subFrame.place(anchor="nw", x=0, y=300, width=200, height=40) # places submit button frame (middle left, under leftFrame)
+subFrame = tk.Frame(root, width=400, height=50, relief="sunken", borderwidth=5) # creates submit button frame
+subFrame.place(anchor="nw", x=0, y=300, width=400, height=60) # places submit button frame (middle left, under leftFrame)
 
 rightFrame = tk.Frame(root, width=400, height=350, relief="raised", borderwidth=5) # creates (currently) empty frame
-rightFrame.place(anchor="nw", x=400, y=0, width=400, height=350) # places frame (top right)
+rightFrame.place(anchor="nw", x=400, y=0, width=400, height=700) # places frame (top right)
+
 '''
 VARIABLES
 '''
@@ -58,13 +59,14 @@ POST-SUBMIT WIDGETS FUNCTION
 ''' 
 def submitWidgets():
     tk.Label(rightFrame, text="Text entered:", font=(font, 17), relief="groove").pack(side="top", anchor="w")
-    tk.Label(rightFrame, text=preText, font=(font, 15), width=1, height=1).pack(side="top", anchor="w", fill="both", expand=1)
+    preLab = tk.Label(rightFrame, text=preText, font=(font, 15), wraplength=375, justify="left")
+    preLab.pack(side="top", anchor="w", fill="x")
     print(f"\nSubmitted Text: {preText}")
     tk.Label(rightFrame, text="Regex results:", font=(font, 17), relief="groove").pack(side="top", anchor="w")
-    tk.Label(rightFrame, text=mySeq, font=(font, 15), width=1, height=1).pack(side="top", anchor="w", fill="both", expand=1)
-    tk.Label(rightFrame, text="Closest match:", font=(font, 17), relief="groove").pack(side="top", anchor="w")
+    seqLab = tk.Label(rightFrame, text=mySeq, font=(font, 15), wraplength=375, justify="left")
+    seqLab.pack(side="top", anchor="w", fill="x")
     print(f"\nRegex Results:{mySeq}")
-    
+    tk.Label(rightFrame, text="Closest match:", font=(font, 17), relief="groove").pack(side="top", anchor="w")
 
 '''
 BLAST FUNCTION
@@ -83,7 +85,8 @@ def blasting():
     hit = blast_record[0]
     org = hit.target.description
     print(f"\nClosest Match: {org}")
-    tk.Label(rightFrame, text=org, font=(font, 15)).pack(side="top",anchor="w")
+    matchLab = tk.Label(rightFrame, text=org, font=(font, 15), wraplength=375, justify="left")
+    matchLab.pack(side="top", anchor="w")
 '''
 SUBMISSION FUNCTION
 -
@@ -112,14 +115,15 @@ APP WINDOW FUNCTION
 '''
 def appWin():
     global textInput
+    #scroll = tk.Scrollbar(rightFrame, orient="vertical")
+    #scroll.pack(side="right", fill="y")
     '''LEFT SIDE'''
     root.resizable(True, True) # makes window resizable
     tk.Label(leftFrame, text="Please enter the text\nyou want sequenced here:", font=(font, 17)).pack(side="top") # creates and places label for text entry
     textInput = tk.Text(leftFrame, height=25, width=50, font=(font, 15)) # creates text entry
     textInput.pack(side="bottom", fill="x") # places text entry
-    submit = tk.Button(subFrame, font=(font, 17), text="Submit", command=submitFunc) # creates submission button
-    submit.pack(fill="both") # places submission button
+    submit = tk.Button(subFrame, font=(font, 30), text="Submit", command=submitFunc, padx=0,pady=0, height=60, width=400) # creates submission button
+    submit.pack(anchor="center") # places submission button
     '''RIGHT SIDE'''
-    
     root.mainloop() # calls Tk main loop 
 appWin() # calls appWin
